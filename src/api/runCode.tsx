55340dcd4a7bee54f79f2603f4d7995bc36c4450
Ivan Holotsvan <iholotsvan@gmail.com>
2025-01-03 12:41:56 +0200

    small fix

--- src/api/runCode.tsx
+++ src/api/runCode.tsx
@@ -1,3 +1,5 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
     if (req.method === 'POST') {
       const { code } = req.body;
